8e988a99c1162da64c9329b42d75a3183e236b99
Dario-Miccinelli <poduzzo@hotmail.com>
2022-11-10 15:52:40 +0100

    end;  including bonus;

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+      message: 'Hello Vue! Welcome!',
+      messagefoot: 'Created in Boolean',
+      image: 'https://images.unsplash.com/photo-1599041125553-a3b967646ca5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&q=80'
+    }
+  })
